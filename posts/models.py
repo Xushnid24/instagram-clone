93a585b6c707bdf56118da499c721b6251458d78
Xushnid Nuratdinov <husnidnuratdinov@gmail.com>
2025-11-12 10:18:01 +0300

    Add Post model and views

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/')
+    caption = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username}: {self.caption[:20]}"
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/post_list.html', {'posts': posts})
